ec6d5a315e6ed6e939245034550cb553e5770100
Nguyễn Phú Cường <npcuong.011308@gmail.com>
2018-05-17 14:02:10 +0700

    Adjust sidebar js and css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,31 +25,56 @@ $(document).ready(function() {
     menu: '#navbar',
     scrollBar: true,
   });
-
   // add or remove active class for sidebar element
   // base on current section
   function activeClass(selector) {
     var sideBar = $(selector);
     var url = window.location.href;
     var id = url.substring(url.lastIndexOf('/') + 1);
+    if (!id)
+      id = "#home";
 
     sideBar.each(function() { 
+      var imgSrc = $("img", this).attr('src'),
+          active = 'active-',
+
+          lastSlashIdx = imgSrc.lastIndexOf('/'),
+          base = imgSrc.substring(0, lastSlashIdx + 1),
+          imgName = imgSrc.substring(lastSlashIdx + 1),
+          newName = imgSrc;
+    // console.log(imgName);
+    // is not active img
+
+    // is not active img
+    // if (imgSrc.search(active) != -1) {
+    //   newName = base + imgName.substring(active.lenght + 1);
+    // }
       if ($(this).attr('href') == id) {
-        $(this).addClass('active');
+        if (imgSrc.search(active) == -1) {
+          newName = base + active + imgName;
+        }
+        $(this).parent().addClass('active');
       }
+
       else {
-        $(this).removeClass('active');
+        // not active section but have active in name
+        if (imgSrc.search(active) != -1) {
+          newName = base + imgName.substring(active.length);
+        }
+        $(this).parent().removeClass('active');
       }
+
+      $("img", this).attr('src', newName);
       $(this).blur();
     });
   }
 
   $(window).load(function() {
-    activeClass('.sidebar ul li a');
+    activeClass('.sidebar ul li a.nav-icon');
   });
 
   $(window).scroll(function() {
-    activeClass('.sidebar ul li a');
+    activeClass('.sidebar ul li a.nav-icon');
   });
 
 });
