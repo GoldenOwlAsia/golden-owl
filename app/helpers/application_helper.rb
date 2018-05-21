626697dc9b29c4c2fd54c1646f4e70ee8d446e9d
Nguyễn Phú Cường <npcuong.011308@gmail.com>
2018-05-21 16:40:08 +0700

    Add fb chat api and dotenv gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def fb_page_id
+    Rails.application.secrets.fb_page_id
+  end
+
+  def google_api_key
+    Rails.application.secrets.google_api_key
+  end
+
+  def a(name)
+    "cuong"
+  end
 end
