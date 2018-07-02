fc767f2a10717edbdad5b03078b98eb61b81684a
Nguyễn Phú Cường <npcuong.011308@gmail.com>
2018-07-02 09:40:24 +0700

    Remove touch event

--- app/assets/javascripts/slide_event.js
+++ app/assets/javascripts/slide_event.js
@@ -1,26 +1,26 @@
 // this js file involves in slide action performences (swiping, using arrow key)
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  var carousel = document.getElementById('myCarousel');
-  // handling arrow keys pressing event
-  $(document).keydown(function(event) {
-    if (event.keyCode == 39) {
-      $(carousel).carousel('next');
-    }
-    if (event.keyCode == 37) {
-      $(carousel).carousel('prev');
-    }
-  });
+//   var carousel = document.getElementById('myCarousel');
+//   // handling arrow keys pressing event
+//   $(document).keydown(function(event) {
+//     if (event.keyCode == 39) {
+//       $(carousel).carousel('next');
+//     }
+//     if (event.keyCode == 37) {
+//       $(carousel).carousel('prev');
+//     }
+//   });
 
-  // handling swiping event
+//   // handling swiping event
 
-  delete Hammer.defaults.cssProps.userSelect; // select text
+//   delete Hammer.defaults.cssProps.userSelect; // select text
 
-  Hammer(carousel).on('swipeleft', function() {
-    $(carousel).carousel('next');
-  });
+//   Hammer(carousel).on('swipeleft', function() {
+//     $(carousel).carousel('next');
+//   });
 
-  Hammer(carousel).on('swiperight', function() {
-    $(carousel).carousel('prev');
-  });
-});
+//   Hammer(carousel).on('swiperight', function() {
+//     $(carousel).carousel('prev');
+//   });
+// });
